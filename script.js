b10c576ba41c32e55cc3d0386353829f9eeadf97
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2021-01-25 19:05:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function alterar() {
+  document.getElementById("title").innerHTML = "Trocou título";
+  document.getElementById("camp").placeholder = "Trocou placeholder";
+};
+
+alterar();
+alert("Função alterar feita com sucesso!");
